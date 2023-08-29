5f13d8205e975ac9913ca5c1f0b96dd373145718
Ruben Gonzalez <neorubenx@gmail.com>
2023-08-29 16:34:05 +0300

    mod1

--- themes.h
+++ themes.h
@@ -5,7 +5,7 @@
 
 /* custom colors */
 #define BLACK                   "#000000"
-#define BLACK4                  "#000000"
+#define BLACK5                  "#000000"
 #define WHITE                   "#FFFFFF"
 #define RED                     "#FF0000"
 #define DARK_RED                "#8B0000"

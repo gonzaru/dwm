d6864c808c4c4d9790cf64daf56d087e92ea4af0
Ruben Gonzalez <neorubenx@gmail.com>
2023-08-30 01:34:09 +0300

    update

--- themes.h
+++ themes.h
@@ -4,7 +4,7 @@
  */
 
 /* custom colors */
-#define BLACK                   "#000021"
+#define BLACK                   "#000022"
 #define WHITE12                   "#FFFFFF"
 #define RED                     "#FF0000"
 #define DARK_RED                "#8B0000"
